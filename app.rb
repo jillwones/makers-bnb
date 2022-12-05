5d2618d48311afd3de4badc1f61d578bc120710e
Aimee <aimeehorwood@hotmail.com>
2022-12-05 17:52:28 +0000

    Adds Listing classes, form to add listings and tests

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require_relative 'lib/database_connection'
 require 'bcrypt'
+require_relative 'lib/listing_repository'
 
 DatabaseConnection.connect
 
@@ -14,5 +15,36 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
-end
 
+
+  get '/listings' do
+    @listing_repository = ListingRepository.new
+    return erb(:listings)
+  end
+
+  get '/listings/:id' do
+    listing_repository = ListingRepository.new
+    @listing = listing_repository.find_by_id(params[:id])
+
+    return erb(:listings_id)
+  end
+
+  get '/new_listing_form' do
+    return erb(:new_listing_form)
+  end
+
+  post '/new_listing_form' do
+    new_listing = Listing.new
+    new_listing.name = params[:name]
+    new_listing.description = params[:description]
+    new_listing.price_per_night = params[:price_per_night]
+    # change below to 'new_listing.user_id = session[:user].id' once sessions added
+    new_listing.user_id = params[:user_id]
+    
+    listing_repository = ListingRepository.new
+    listing_repository.create(new_listing)
+    id = listing_repository.all.last.id
+
+    return redirect "/listings/#{id}"
+  end
+end
---
+++ spec/listing_repository_spec.rb
@@ -0,0 +1,72 @@
+require "listing_repository"
+require "listing"
+
+def reset_tables
+  seed_sql = File.read("spec/seeds.sql")
+  connection = PG.connect({ host: "127.0.0.1", dbname: "makersbnb_test" })
+  connection.exec(seed_sql)
+end
+
+describe ListingRepository do
+  before(:each) do
+    reset_tables
+  end
+
+  context "all method" do
+    it "returns all listings" do
+      repo = ListingRepository.new
+      listings = repo.all
+
+      expect(listings.length).to eq 6
+      expect(listings.first.id).to eq "1"
+      expect(listings.first.name).to eq "Apartment1"
+      expect(listings.first.description).to eq "Two bedrooms in north London"
+      expect(listings.first.price_per_night).to eq "100"
+      expect(listings.first.user_id).to eq "1"
+      expect(listings.last.id).to eq "6"
+      expect(listings.last.name).to eq "Apartment3"
+      expect(listings.last.description).to eq "Studio flat in central London"
+      expect(listings.last.price_per_night).to eq "85.75"
+      expect(listings.last.user_id).to eq "1"
+    end
+  end
+
+  context "create method" do
+    it "creates a new listing which gets added to the database" do
+      repo = ListingRepository.new
+      listing = Listing.new
+      listing.name = "test listing"
+      listing.description = "test description"
+      listing.price_per_night = 123.45
+      listing.user_id = 1
+      repo.create(listing)
+      newest_listing = repo.all.last
+      expect(newest_listing.name).to eq "test listing"
+      expect(newest_listing.description).to eq "test description"
+      expect(newest_listing.price_per_night).to eq "123.45"
+      expect(newest_listing.user_id).to eq "1"
+    end
+  end
+
+  context "delete method" do
+    it "deletes a listing" do
+      repo = ListingRepository.new
+      repo.delete(1)
+      listings = repo.all
+
+      expect(listings.length).to eq(5)
+      expect(listings.first.id).to eq("2")
+    end
+  end
+
+  context "find_by_id method" do
+    it "finds a single listing by id" do
+      repo = ListingRepository.new
+      listing = repo.find_by_id(1)
+      expect(listing.id).to eq '1'
+      expect(listing.name).to eq 'Apartment1'
+      expect(listing.description).to eq 'Two bedrooms in north London'
+      expect(listing.price_per_night).to eq '100'
+    end  
+  end  
+end
