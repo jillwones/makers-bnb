6e0a560edc8ba1f0638f991e43e07002472a9c95
henrychow <113641610+henrow5@users.noreply.github.com>
2022-12-08 12:26:43 +0000

    Add request button

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require_relative "lib/booking_repository"
 require_relative "lib/date_repository"
 require_relative "lib/twilio.rb"
 require "bcrypt"
-require 'date'
+require "date"
 
 DatabaseConnection.connect
 
@@ -52,46 +52,71 @@ class Application < Sinatra::Base
     redirect "/"
   end
 
-  get '/signup' do 
+  get "/signup" do
     @message = session.delete(:message)
     return erb(:signup)
   end
 
-  post '/signup' do 
-    @user_repository = UserRepository.new 
-    new_user = User.new 
+  post "/signup" do
+    @user_repository = UserRepository.new
+    new_user = User.new
     new_user.name = params[:name]
-    new_user.email_address = params[:email_address] 
+    new_user.email_address = params[:email_address]
     new_user.password = params[:password]
     taken = @user_repository.check_if_email_taken(params[:email_address])
-    if taken 
-      session[:message] = 'Email taken - use another'
-      return redirect '/signup'
+    if taken
+      session[:message] = "Email taken - use another"
+      return redirect "/signup"
     end
-    session[:message] = 'Account Successfully Created - Please Log In'
+    session[:message] = "Account Successfully Created - Please Log In"
     @user_repository.create(new_user)
-    return redirect '/'
+    return redirect "/"
   end
 
-  get '/listings' do
+  get "/listings" do
     @listing_repository = ListingRepository.new
     return erb(:listings)
   end
 
-  get '/listings/:id' do
+  get "/listings/:id" do
+    @message = session.delete(:message)
     listing_repository = ListingRepository.new
     date_repository = DateRepository.new
- 
+
     @listing = listing_repository.find_by_id(params[:id])
     @dates = date_repository.find_by_listing_id(params[:id])
     return erb(:listings_id)
   end
 
-  get '/new_listing_form' do
+  post "/listings/:id" do
+    listing_repository = ListingRepository.new
+    date_repository = DateRepository.new
+
+    @listing = listing_repository.find_by_id(params[:id])
+    @dates = date_repository.find_by_listing_id(params[:id])
+
+    if @dates.map(&:date_available).include?(params[:date])
+      booking = Booking.new
+      booking_repository = BookingRepository.new
+
+      booking.name = @listing.name
+      booking.date = params[:date]
+      booking.booked = "pending"
+      booking.user_id = session[:user].id
+      booking.listing_id = params[:id]
+      booking_repository.create(booking)
+      return redirect "/my-requests"
+    else
+      session[:message] = "Date unavailable, choose a different date."
+      return redirect "/listings/#{params[:id]}"
+    end
+  end
+
+  get "/new_listing_form" do
     return erb(:new_listing_form)
   end
 
-  post '/new_listing_form' do
+  post "/new_listing_form" do
     new_listing = Listing.new
     new_listing.name = params[:name]
     new_listing.description = params[:description]
@@ -102,44 +127,44 @@ class Application < Sinatra::Base
     listing_repository = ListingRepository.new
     listing_repository.create(new_listing)
     id = listing_repository.all.last.id
-    new_listing.id = id 
-    dates_available_repo = DateRepository.new 
+    new_listing.id = id
+    dates_available_repo = DateRepository.new
     new_listing.dates_available.each do |date|
       dates_available_repo.create(new_listing, date)
     end
     return redirect "/listings/#{id}"
   end
 
-  get '/my-requests' do 
+  get "/my-requests" do
     @booking_repository = BookingRepository.new
     return erb(:my_requests)
   end
 
-  get '/requests-for-approval' do 
+  get "/requests-for-approval" do
     @booking_repository = BookingRepository.new
     @user_repository = UserRepository.new
     return erb(:requests_for_approval)
   end
 
-  post '/accept/:request_id/:listing_id/:request_date/:user_id' do 
-    @booking_repository = BookingRepository.new 
+  post "/accept/:request_id/:listing_id/:request_date/:user_id" do
+    @booking_repository = BookingRepository.new
     @booking_repository.accept(params[:request_id])
-    dates_available_repo = DateRepository.new 
-    dates_available_repo.delete_by_listing_id_and_date(params[:listing_id],params[:request_date])
-    user_repository = UserRepository.new 
+    dates_available_repo = DateRepository.new
+    dates_available_repo.delete_by_listing_id_and_date(params[:listing_id], params[:request_date])
+    user_repository = UserRepository.new
     user = user_repository.find_by_id(params[:user_id])
-    text = Text.new 
-    text.send_text_approved('+447379766090')
-    return redirect '/requests-for-approval'
+    text = Text.new
+    text.send_text_approved("+447379766090")
+    return redirect "/requests-for-approval"
   end
 
-  post '/reject/:id' do 
-    @booking_repository = BookingRepository.new 
+  post "/reject/:id" do
+    @booking_repository = BookingRepository.new
     @booking_repository.decline(params[:id])
-    return redirect '/requests-for-approval'
+    return redirect "/requests-for-approval"
   end
 
-  get '/my-listings' do 
+  get "/my-listings" do
     @listing_repository = ListingRepository.new
     return erb(:my_listings)
   end
@@ -147,7 +172,7 @@ class Application < Sinatra::Base
   def method_to_make_multiple_dates(listing, start_date, end_date)
     start_date = Date.parse start_date
     end_date = Date.parse end_date
-    while start_date <= end_date do 
+    while start_date <= end_date
       listing.dates_available << start_date
       start_date += 1
     end
--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -46,22 +46,22 @@ RSpec.describe Application do
     end
   end
 
-  context 'GET /signup' do 
-    it 'returns the signup page' do 
-      response = get('/signup')
+  context "GET /signup" do
+    it "returns the signup page" do
+      response = get("/signup")
 
       expect(response.status).to eq(200)
-      expect(response.body).to include('Enter your details below')
+      expect(response.body).to include("Enter your details below")
     end
   end
 
-  context 'POST /signup' do 
-    it 'returns the login page with success message' do 
-      post('/signup', name: 'Freddy', email_address: 'freddy@freddy.com', password: 'freddy')
-      response = get('/')
+  context "POST /signup" do
+    it "returns the login page with success message" do
+      post("/signup", name: "Freddy", email_address: "freddy@freddy.com", password: "freddy")
+      response = get("/")
 
       expect(response.status).to eq(200)
-      expect(response.body).to include('Account Successfully Created - Please Log In')
+      expect(response.body).to include("Account Successfully Created - Please Log In")
     end
   end
 
@@ -98,119 +98,140 @@ RSpec.describe Application do
     end
   end
 
-  context "get /new_listing_form" do 
+  xcontext "post /listings/:id" do
+    it "submits a booking request and redirects to 'my-requests'" do
+      response = post("/",
+                      email: "aimee@aimee.com",
+                      password: "aimee")
+
+      expect(response.status).to eq(302)
+
+      response = post("/listings/1",
+                      date: "2023-10-11")
+
+      expect(response.status).to eq(200)
+
+      response = get("/my-requests")
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include("<p>Name: Apartment1</p>")
+      expect(response.body).to include("<p>Date Requested: 2023-10-11</p>")
+      expect(response.body).to include("<p><b>Status: Pending</b></p>")
+    end
+  end
+
+  context "get /new_listing_form" do
     it "returns 200 OK and a form to create new listing" do
       response = get("/new_listing_form")
       expect(response.status).to eq 200
       expect(response.body).to include('<form action="/new_listing_form" method="post">')
       expect(response.body).to include('<label for="name">Name:</label><br>')
-    end 
+    end
   end
 
   context "post /new_listing_form" do
     xit "returns 200 OK and adds listing to database" do
-      post('/',
-        email_address: 'aimee@aimee.com',
-        password: 'aimee')
+      post("/",
+           email_address: "aimee@aimee.com",
+           password: "aimee")
       post(
         "/new_listing_form",
         name: "Loft",
         description: "Loft conversion in South London",
         price_per_night: "150",
-        user_id: '2',
+        user_id: "2",
         start_date: "2022-12-25",
-        end_date: "2022-12-29"
+        end_date: "2022-12-29",
       )
-      
-      response = get("/listings/7")
-      
-      expect(response.status).to eq 200
-      expect(response.body).to include('<p>Listing id: 7</p>')
-      expect(response.body).to include('<p>Loft</p>')
-      expect(response.body).to include('<p>Loft conversion in South London</p>')
-      expect(response.body).to include('<p>150</p>')
-      expect(response.body).to include('<p>User id: 2</p>')
 
+      response = get("/listings/7")
+
+      expect(response.status).to eq 200
+      expect(response.body).to include("<p>Listing id: 7</p>")
+      expect(response.body).to include("<p>Loft</p>")
+      expect(response.body).to include("<p>Loft conversion in South London</p>")
+      expect(response.body).to include("<p>150</p>")
+      expect(response.body).to include("<p>User id: 2</p>")
 
       response = get("/listings")
-      
+
       expect(response.status).to eq 200
-      expect(response.body).to include('<p>Listing id: 7</p>')
-      expect(response.body).to include('<p>Loft</p>')
-      expect(response.body).to include('<p>Loft conversion in South London</p>')
-      expect(response.body).to include('<p>150</p>')
-      expect(response.body).to include('<p>User id: 2</p>')
-    end 
-  end
-
-  context 'get /my-requests' do
-    it 'returns a page with all requests that a logged in user has made' do
-      response = post('/',
-        email_address: 'aimee@aimee.com',
-        password: 'aimee')
-
-      expect(response.status).to eq(302)
-      
-      response = get('/my-requests')
-
-      expect(response.status).to eq(200)
-      expect(response.body).to include('<h2>Pending:</h2>')
-      expect(response.body).to include('<p>Name: Apartment3</p>')
-      expect(response.body).to include('<p>Date Requested: 2023-12-11</p>')
-      expect(response.body).to include('<p><b>Status: Pending</b></p>')
-      expect(response.body).to include('<h2>Accepted:</h2>')
-      expect(response.body).to include('<p>Name: Apartment1</p>')
-      expect(response.body).to include('<p>Date Requested: 2023-10-10</p>')
-      expect(response.body).to include('<p><b>Status: Accepted</b></p>')
-      expect(response.body).to include('<h2>Rejected:</h2>')
-      expect(response.body).to include('<p>No rejected bookings</p>')
+      expect(response.body).to include("<p>Listing id: 7</p>")
+      expect(response.body).to include("<p>Loft</p>")
+      expect(response.body).to include("<p>Loft conversion in South London</p>")
+      expect(response.body).to include("<p>150</p>")
+      expect(response.body).to include("<p>User id: 2</p>")
     end
   end
 
-  context 'get /requests-for-approval' do
+  context "get /my-requests" do
+    it "returns a page with all requests that a logged in user has made" do
+      response = post("/",
+                      email_address: "aimee@aimee.com",
+                      password: "aimee")
+
+      expect(response.status).to eq(302)
+
+      response = get("/my-requests")
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include("<h2>Pending:</h2>")
+      expect(response.body).to include("<p>Name: Apartment3</p>")
+      expect(response.body).to include("<p>Date Requested: 2023-12-11</p>")
+      expect(response.body).to include("<p><b>Status: Pending</b></p>")
+      expect(response.body).to include("<h2>Accepted:</h2>")
+      expect(response.body).to include("<p>Name: Apartment1</p>")
+      expect(response.body).to include("<p>Date Requested: 2023-10-10</p>")
+      expect(response.body).to include("<p><b>Status: Accepted</b></p>")
+      expect(response.body).to include("<h2>Rejected:</h2>")
+      expect(response.body).to include("<p>No rejected bookings</p>")
+    end
+  end
+
+  context "get /requests-for-approval" do
     it "returns a page with all requests on a user's listings" do
-      response = post('/',
-        email_address: 'jude@jude.com',
-        password: 'jude')
+      response = post("/",
+                      email_address: "jude@jude.com",
+                      password: "jude")
 
       expect(response.status).to eq(302)
-      
-      response = get('/requests-for-approval')
+
+      response = get("/requests-for-approval")
 
       expect(response.status).to eq(200)
-      expect(response.body).to include('<h1>Requests for Approval</h1>')
-      expect(response.body).to include('<h2>Pending:</h2>')
-      expect(response.body).to include('<p>Date Requested: 2023-12-11</p>')
-      expect(response.body).to include('<p><b>Status: Accepted</b></p>')
-      expect(response.body).to include('<p>Name of requester: Aimee</p>')
-      expect(response.body).to include('<p>Email of requester: aimee@aimee.com</p>')
-      expect(response.body).to include('<p>Name: Apartment3</p>')
-      expect(response.body).to include('<h2>Accepted:</h2>')
-      expect(response.body).to include('<p>Date Requested: 2023-10-10</p>')
-      expect(response.body).to include('<p><b>Status: Accepted</b></p>')
-      expect(response.body).to include('<p>Name of requester: Aimee</p>')
-      expect(response.body).to include('<p>Email of requester: aimee@aimee.com</p>')
-      expect(response.body).to include('<p>Name: Apartment1</p>')
-      expect(response.body).to include('<h2>Rejected:</h2>')
-      expect(response.body).to include('<p>No rejected bookings</p>')
+      expect(response.body).to include("<h1>Requests for Approval</h1>")
+      expect(response.body).to include("<h2>Pending:</h2>")
+      expect(response.body).to include("<p>Date Requested: 2023-12-11</p>")
+      expect(response.body).to include("<p><b>Status: Accepted</b></p>")
+      expect(response.body).to include("<p>Name of requester: Aimee</p>")
+      expect(response.body).to include("<p>Email of requester: aimee@aimee.com</p>")
+      expect(response.body).to include("<p>Name: Apartment3</p>")
+      expect(response.body).to include("<h2>Accepted:</h2>")
+      expect(response.body).to include("<p>Date Requested: 2023-10-10</p>")
+      expect(response.body).to include("<p><b>Status: Accepted</b></p>")
+      expect(response.body).to include("<p>Name of requester: Aimee</p>")
+      expect(response.body).to include("<p>Email of requester: aimee@aimee.com</p>")
+      expect(response.body).to include("<p>Name: Apartment1</p>")
+      expect(response.body).to include("<h2>Rejected:</h2>")
+      expect(response.body).to include("<p>No rejected bookings</p>")
     end
   end
 
-  context '/accept/:request_id/:listing_id/:request_date/:user_id' do
+  context "/accept/:request_id/:listing_id/:request_date/:user_id" do
     xit "changes a pending request to accepted" do
-      response = post('/accept/4/3/2022-10-20/4')
+      response = post("/accept/4/3/2022-10-20/4")
 
       expect(response.status).to eq(302)
-      expect(response.body).not_to include('Status: Pending')
+      expect(response.body).not_to include("Status: Pending")
     end
   end
 
-  context '/reject/:id' do
-    it 'changes pending request to rejected' do
-      response = post('/reject/4')
+  context "/reject/:id" do
+    it "changes pending request to rejected" do
+      response = post("/reject/4")
 
       expect(response.status).to eq(302)
-      expect(response.body).not_to include('Status: Pending')
+      expect(response.body).not_to include("Status: Pending")
     end
   end
 end
